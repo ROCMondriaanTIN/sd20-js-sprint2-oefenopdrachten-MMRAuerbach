b0ba863c0993be1f288f0c1d3f502b58e7640d07
MMR Auerbach <m.auerbach@rocmondriaan.nl>
2021-03-11 14:51:21 +0100

    update solution

--- js/page_5.js
+++ js/page_5.js
@@ -1,9 +1,13 @@
 
 
-let theButton = document.querySelector('.my-first-button');
+const theButton = document.querySelector('.my-first-button');
+const studentNumber = 54321; 
+const myCity = 'Den Haag'; 
+
 if (theButton) {
     theButton.addEventListener('click', function() {
-        const yourName = prompt('Wat is je naam?', '');
-        console.log(`De gekozen naam is: ${yourName} `);
-    })
+        const yourName = prompt('Wat is je naam?', "");
+        console.log('De gekozen naam is: ' + yourName + ' en je studenten nummer is ' + studentNumber + ' en je stad is .... ' + myCity);
+        console.log(`De gekozen naam is: ${yourName} en je studenten nummer is ${studentNumber} en je stad is .... ${myCity}`);
+    });
 }
